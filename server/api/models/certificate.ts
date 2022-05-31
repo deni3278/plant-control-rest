d03ad9ece0f3f751b17912b3cb2ceca338dafe0a
CKDenni <deni3278@easv365.dk>
2022-05-31 11:29:28 +0200

    Add Certificate model

---
+++ server/api/models/certificate.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+import sequence from "mongoose-sequence";
+import {IPairingModel} from "./pairing";
+
+const AutoIncrement = sequence(mongoose);
+
+export interface ICertificateModel extends mongoose.Document {
+    id: number;
+    createdAt: Date;
+    pairing: IPairingModel;
+}
+
+const schema = new mongoose.Schema(
+    {
+        id: { type: Number, unique: true, required: true },
+        createdAt: { type: Date, required: true },
+        pairing: { type: mongoose.Schema.Types.ObjectId, ref: 'Pairing', required: true }
+    },
+    {
+        collection: "certificates",
+    }
+);
+
+schema.plugin(AutoIncrement, { inc_field: "id" });
+
+export const Certificate = mongoose.model<ICertificateModel>("Certificate", schema);
