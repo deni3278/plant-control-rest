a5fce39c611331743bc72530d56f4070b5e3bedc
deni3278 <deni3278@easv365.dk>
2022-06-04 11:31:49 +0200

    Refactor

--- src/base.service.ts
+++ src/base.service.ts
@@ -1,37 +1,37 @@
-import { Model } from "mongoose";
+import {Model} from "mongoose";
 
 
 export abstract class BaseService<T> {
-  private _repository: Model<T>;
-  private _populateOnFind: { path: string, model: Model<any> }[];
+    private _repository: Model<T>;
+    private _populateOnFind: { path: string, model: Model<any> }[];
 
-  constructor(repository: Model<T>, populateOnFind: { path: string, model: Model<any> }[] = []) {
-    this._repository = repository;
-    this._populateOnFind = populateOnFind;
-  }
+    constructor(repository: Model<T>, populateOnFind: { path: string, model: Model<any> }[] = []) {
+        this._repository = repository;
+        this._populateOnFind = populateOnFind;
+    }
 
-  async findAll(): Promise<T[]> {
+    async findAll(): Promise<T[]> {
 
-    return this._repository.find().populate(this._populateOnFind).exec();
-  }
+        return this._repository.find().populate(this._populateOnFind).exec();
+    }
 
-  async findOne(id: any): Promise<T> {
+    async findOne(id: any): Promise<T> {
 
-    const leanDoc = await this._repository.findById(id).populate(this._populateOnFind).exec();
-    return leanDoc as T;
-  }
+        const leanDoc = await this._repository.findById(id).populate(this._populateOnFind).exec();
+        return leanDoc as T;
+    }
 
-  async create(item: T): Promise<T> {
+    async create(item: T): Promise<T> {
 
-    return this._repository.create(item);
-  }
+        return this._repository.create(item);
+    }
 
-  async update(id: string, item: T) {
+    async update(id: string, item: T) {
 
-    return this._repository.findByIdAndUpdate(id, item);
-  }
+        return this._repository.findByIdAndUpdate(id, item);
+    }
 
-  async remove(id: string) {
-    return this._repository.findByIdAndRemove(id);
-  }
+    async remove(id: string) {
+        return this._repository.findByIdAndRemove(id);
+    }
 }
