ad0ac527e1f82c8084ec63c960dcc4c506accecf
Rasmus <66802136+rasmus234@users.noreply.github.com>
2022-06-15 20:47:09 +0200

    test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,31 +1,37 @@
-import { Test, TestingModule } from "@nestjs/testing";
-import { INestApplication } from "@nestjs/common";
+import {Test, TestingModule} from "@nestjs/testing";
+import {INestApplication} from "@nestjs/common";
 import * as request from "supertest";
-import { AppModule } from "../src/app.module";
+import {AppModule} from "../src/app.module";
 import mongoose from "mongoose";
+import {LoggerDocument} from "../src/logger/logger.schema";
 
 describe("AppController (e2e)", () => {
-  jest.setTimeout(30000);
-  let app: INestApplication;
+    jest.setTimeout(30000);
+    let app: INestApplication;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule]
-    }).compile();
+    beforeEach(async () => {
+        const moduleFixture: TestingModule = await Test.createTestingModule({
+            imports: [AppModule]
+        }).compile();
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
+        app = moduleFixture.createNestApplication();
+        await app.init();
+    });
 
-  it("/ (GET)", async () => {
-    return request(app.getHttpServer())
-        .get("/loggers")
-        .expect(200);
-  });
+    it("/ (GET)", async () => {
+        return request(app.getHttpServer())
+            .get("/loggers", (err, res) => {
+                const data: LoggerDocument[] = res.body;
+                expect(data[0].id).toBeDefined();
+                expect(res.status).toBe(200);
+            })
 
-  afterAll(done => {
-    // Closing the DB connection allows Jest to exit successfully.
-    mongoose.connection.close()
-    done()
-  })
+
+    });
+
+    afterAll(done => {
+        // Closing the DB connection allows Jest to exit successfully.
+        mongoose.connection.close()
+        done()
+    })
 });
